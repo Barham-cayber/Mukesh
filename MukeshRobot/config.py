38ed0b175af3197503025a0540d6a105d7c9b2b5
Barham-cayber <154244646+Barham-cayber@users.noreply.github.com>
2024-09-07 12:42:19 +0300

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -2,15 +2,15 @@ class Config(object):
     LOGGER = True
     API_ID =27442758
     API_HASH = "8288c5c54f568ee346b3227271700b52"
-    TOKEN = "7104421875:AAHw9a8fqjhyNAiUtyAb-HNMvzfvZOkq0gA"  
+    TOKEN = "7104421875:AAHk0NmGYO3lMZI69CbZwuLDi6NJtCuchWw"  
     OWNER_ID=5576431780
     
     SUPPORT_CHAT = "-1002100221457" 
     START_IMG = "https://t.me/photosmyys/2"
     EVENT_LOGS = (-1002032682720)
-    MONGO_DB_URI= "mongodb+srv://<Barhammukesh>:<Barham1234>@cluster0.nwnf3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_DB_URI= "mongodb+srv://barham7726:Barham1234@cluster0.ll412.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
    
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://avnadmin:AVNS_NusHYCfUDFlQ7kRdxri@joinbot-barhammhamad7726-593f.i.aivencloud.com:17809/defaultdb?sslmode=require"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
         ""
     )
